b59a304632aa06135798b17cb64f02eb5451cba9
massivebird <gdrakemail@gmail.com>
2024-05-25 07:54:02 -0400

    refactor: created a printa

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let timestamp = chrono::Local::now();
-    println!("[{timestamp:?}] Make sure to set your volume unmuted and low!");
-    println!("[{timestamp:?}] Let me do my thing");
+    let my_print = |msg: &str| {
+        let timestamp = chrono::Local::now();
+        println!("[{timestamp:?}] {msg}");
+    };
+    my_print("Make sure to set your volume unmuted and low!");
+    my_print("Let me do my thing");
 
     let alarm_on_loop = || loop {
         Command::new("mpv")
@@ -19,12 +22,10 @@ fn main() {
     loop {
         let html = String::from_utf8(Command::new("curl").arg("https://bannerweb.oci.emich.edu/pls/banner/bwckschd.p_disp_detail_sched?term_in=202510&crn_in=13153").output().unwrap().stdout).unwrap();
 
-        let timestamp = chrono::Local::now();
-
         let re = regex::Regex::new(r"dddefault.>[^30]{1,2}[0]{0,1}</td>").unwrap();
 
         if re.is_match(&html) {
-            println!("[{timestamp:?}] NONTHIRTY, NONZERO VALUE DETECTED!!!!");
+            my_print("NONTHIRTY, NONZERO VALUE DETECTED!!!!");
             alarm_on_loop();
         }
 
