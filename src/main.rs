efa6b02a7fdc0ea6495b26dfc7e42d6a217f2417
massivebird <gdrakemail@gmail.com>
2024-08-21 18:39:56 -0400

    feat: log crate

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
+use rand::Rng;
 use std::process::{Command, Stdio};
 use std::thread;
 use std::time::Duration;
 
-use rand::Rng;
-
 fn main() {
-    let my_print = |msg: &str| {
-        let timestamp = chrono::Local::now();
-        println!("[{timestamp:?}] {msg}");
-    };
-    my_print("Make sure to set your volume unmuted and low!");
-    my_print("Let me do my thing");
+    simplelog::TermLogger::init(
+        log::LevelFilter::Info,
+        simplelog::Config::default(),
+        simplelog::TerminalMode::Stdout,
+        simplelog::ColorChoice::Always,
+    )
+    .unwrap();
 
-    let song_path = "/home/penguino/Music/Krystle (URL Cyber Palace Mix) [dVsdh98eapI].m4a";
+    log::info!("Make sure to set your volume unmuted and low!");
+    log::info!("Let me do my thing");
+
+    let song_path = "/home/penguino/Music/Machine Girl/Wlfgrl/03 - Machine Girl - Krystle - URL Cyber Palace Mix.mp3";
 
     assert!(
         std::path::Path::new(song_path).exists(),
         "Song file {song_path} does not exist."
     );
 
+    let crns: Vec<u32> = vec![11264, 11265];
+
+    let mut crn_iter = crns.iter().cycle();
+
+    loop {
+        let crn = crn_iter.next().unwrap();
+
+        check_course(crn, &song_path);
+
+        let mut rng = rand::thread_rng();
+        thread::sleep(Duration::from_secs(rng.gen_range(30..=72)));
+    }
+}
+
+fn check_course(crn: &u32, song_path: &str) {
     let alarm_on_loop = || loop {
         Command::new("mpv")
             .arg(song_path)
@@ -28,27 +46,31 @@ fn main() {
         std::thread::sleep(std::time::Duration::from_secs(240));
     };
 
-    let mut rng = rand::thread_rng();
+    let course_catalog_url = format!("https://bannerweb.oci.emich.edu/pls/banner/bwckschd.p_disp_detail_sched?term_in=202510&crn_in={crn}");
 
-    loop {
-        let html = String::from_utf8(Command::new("curl").arg("https://bannerweb.oci.emich.edu/pls/banner/bwckschd.p_disp_detail_sched?term_in=202510&crn_in=14313").output().unwrap().stdout).unwrap();
+    let html = String::from_utf8(
+        Command::new("curl")
+            .arg(course_catalog_url)
+            .output()
+            .unwrap()
+            .stdout,
+    )
+    .unwrap();
 
-        let html_re = regex::Regex::new(r"dddefault").unwrap();
+    let html_re = regex::Regex::new(r"dddefault").unwrap();
 
-        if !html_re.is_match(&html) {
-            my_print("Uh oh. We're not getting HTML anymore.");
-            dbg!(html);
-            thread::sleep(Duration::from_secs(rng.gen_range(72..=248)));
-            continue;
-        }
-
-        let availability_re = regex::Regex::new(r"dddefault.>[^30]{1,2}[0]{0,1}</td>").unwrap();
-
-        if availability_re.is_match(&html) {
-            my_print("NONTHIRTY, NONZERO VALUE DETECTED!!!!");
-            alarm_on_loop();
-        }
-
-        thread::sleep(Duration::from_secs(rng.gen_range(72..=248)));
+    if !html_re.is_match(&html) {
+        log::error!("Uh oh. We're not getting HTML anymore.");
+        dbg!(html);
+        return;
     }
+
+    let availability_re = regex::Regex::new(r"dddefault.>[^30]{1,2}[0]{0,1}</td>").unwrap();
+
+    if availability_re.is_match(&html) {
+        log::warn!("NONTHIRTY, NONZERO VALUE DETECTED FOR COURSE {crn}!!!!");
+        alarm_on_loop();
+    }
+
+    log::info!("CRN {crn} analysis clean.");
 }
